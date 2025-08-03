6ac19a53739c421b7685e5f9bf5034e31777064e
GoSkyH1gh <serbangalea1@gmail.com>
2025-08-03 17:22:48 +0300

    improved look and optimised for remote deploy

--- src/pages/playerComponents/copyIcon.jsx
+++ src/pages/playerComponents/copyIcon.jsx
@@ -1,11 +1,12 @@
 import { motion } from "motion/react"
+import copyIcon from '/src/assets/copy-icon.svg'
 
 function CopyIcon({ textToCopy }) {
   return (
   <motion.img
     whileHover={{ scale: 1.1 }}
     whileTap={{ scale: 0.7, duration: 0.5 }}
-    src='/src/assets/content_copy_24dp_RGB(240, 240, 245)_FILL0_wght400_GRAD0_opsz24.svg'
+    src={copyIcon}
     onClick={() => { navigator.clipboard.writeText(textToCopy)}}
     alt='copy uuid to clipboard'
     className="copy-icon"/>
