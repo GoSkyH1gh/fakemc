7a940169a719a5ec715bd3d9af1f92a756024201
GoSkyH1gh <serbangalea1@gmail.com>
2025-08-20 00:40:20 +0300

    improved logic for player tracker

--- exceptions.py
+++ exceptions.py
@@ -10,3 +10,10 @@ class NotFound(HTTPException):
             detail=f"Requested resource not found",
             status_code=404,
         )
+
+class UpstreamError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            detail=f"Recieved an error from Upstream",
+            status_code=502,
+        )
--- player_tracker.py
+++ player_tracker.py
@@ -6,6 +6,8 @@ import aiohttp
 import os
 from dotenv import load_dotenv
 from utils import dashify_uuid
+import exceptions
+from fastapi import HTTPException
 
 load_dotenv()
 
@@ -18,8 +20,38 @@ if not wynn_token:
 if not hypixel_api_key:
     raise RuntimeError("Hypixel Api key not set in environment vairables.")
 
-trackers: Dict[str, asyncio.Task] = {}  # uuid, poller
-subscribers: Dict[str, Set[asyncio.Queue]] = {}  # uuid, queue
+hypixel_game_type_map = {  # probably not all of them but it's the main ones
+    "Skyblock": "SkyBlock",
+    "Murder_mystery": "Murder Mystery",
+    "Tntgames": "TNT Games",
+    "Build_battle": "Build Battle",
+    "Uhc": "UHC",
+    "Legacy": "Classic Games",
+    "Mcgo": "Cops and Crims",
+    "Walls3": "Mega Walls",
+    "Super_smash": "Smash Heroes",
+    "Battleground": "Warlords",
+    "Survival_games": "Blitz Survival Games",
+}
+
+hypixel_mode_map = {  # only for skyblock atm
+    "Dynamic": "Private Island",
+    "Hub": "Hub",
+    "Dungeon_hub": "Dungeon Hub",
+    "Farming_1": "The Farming Islands",
+    "Foraging_1": "The Park",
+    "Foraging_2": "Moonglade Marsh",
+    "Mining_1": "Gold Mine",
+    "Mining_2": "Deep Caverns",
+    "Mining_3": "Dwarven Mines",
+    "Crystal_hollows": "Crystal Hollows",
+    "Combat_1": "Spider's Den",
+    "Combat_3": "End",
+    "Crimson_isle": "Crimson Isle",
+    "Garden": "Garden",
+    "Rift": "Rift",
+    "Fishing_1": "Backwater Bayou",
+}
 
 
 class PlayerStatus(BaseModel):
@@ -31,19 +63,25 @@ class PlayerStatus(BaseModel):
     hypixel_mode: Optional[str]
 
 
+trackers: Dict[str, asyncio.Task] = {}  # uuid, poller
+subscribers: Dict[str, Set[asyncio.Queue]] = {}  # uuid, queue
+ignored_sources: Dict[str, Set[str]] = {}  # uuid, set["wynncraft", "hypixel"]
+
+
 async def poller(uuid: str):
-    counter = 0
     print(f"poller started for {uuid}")
     try:
         while True:
-            data = await get_status(uuid)
-            message = f"event: data\ndata: {data.model_dump_json()}\n\n"
-            counter += 1
+            try:
+                data = await get_status(uuid)
+                message = f"event: data\ndata: {data.model_dump_json()}\n\n"
+            except:
+                message = "event: error\ndata: {}\n\n"
 
             for subscriber in list(subscribers.get(uuid, [])):
                 await subscriber.put(message)
 
-            await asyncio.sleep(20)
+            await asyncio.sleep(60)
     except asyncio.CancelledError:
         print(f"poller for uuid {uuid} stopped")
     except Exception as e:
@@ -57,6 +95,9 @@ async def subscribe(uuid: str):
         subscribers[uuid] = set()
 
     subscribers[uuid].add(queue)
+
+    if uuid not in ignored_sources:
+        ignored_sources[uuid] = set()
 
     if uuid not in trackers:
         task = asyncio.create_task(poller(uuid))
@@ -73,6 +114,7 @@ def unsubscribe(uuid: str, queue: asyncio.Queue):
 
     if len(subscribers[uuid]) == 0:
         del subscribers[uuid]
+        del ignored_sources[uuid]
 
         if uuid in trackers:
             task = trackers[uuid]
@@ -80,33 +122,71 @@ def unsubscribe(uuid: str, queue: asyncio.Queue):
             del trackers[uuid]
 
 
-async def get_status(uuid):
-
+async def get_status(uuid) -> PlayerStatus:
+    print(f"ignored sources: {ignored_sources[uuid]}")
     async with aiohttp.ClientSession() as session:
         results = await asyncio.gather(
-            get_wynncraft_status(session, uuid), get_hypixel_status(session, uuid)
+            get_wynncraft_status(session, uuid),
+            get_hypixel_status(session, uuid),
+            return_exceptions=True,
         )
-
     wynncraft_response = results[0]
     hypixel_response = results[1]
+
     print(hypixel_response)
 
-    wynncraft_online = wynncraft_response.get("online")
-    wynncraft_server = wynncraft_response.get("server")
-    wynncraft_restricted = wynncraft_response.get("restrictions", {}).get(
-        "onlineStatus", True
-    )
+    if isinstance(wynncraft_response, exceptions.NotFound):
+        print(f"Adding Wynncraft to ignored_sources for uuid {uuid}")
+        ignored_sources[uuid].add("wynncraft")
+    elif isinstance(wynncraft_response, HTTPException):
+        raise exceptions.UpstreamError()
 
-    hypixel_data: dict = hypixel_response.get("session", {})
-    hypixel_online = hypixel_data.get("online", False)
+    if isinstance(hypixel_response, exceptions.NotFound):
+        print(f"Adding Hypixel to ignored_sources for uuid {uuid}")
+        ignored_sources[uuid].add("hypixel")
+    elif isinstance(hypixel_response, HTTPException):
+        raise exceptions.UpstreamError()
 
-    hypixel_game_type = hypixel_data.get("gameType")  # eg SKYBLOCK, PROTOTYPE, BEDWARS
-    if isinstance(hypixel_game_type, str):
-        hypixel_game_type = hypixel_game_type.capitalize()
+    try:
+        wynncraft_online = False
+        wynncraft_server = None
+        wynncraft_restricted = False
+        if wynncraft_response is not None and not isinstance(
+            wynncraft_response, HTTPException
+        ):
+            wynncraft_online = wynncraft_response.get("online")
+            wynncraft_server = wynncraft_response.get("server")
+            wynncraft_restricted = wynncraft_response.get("restrictions", {}).get(
+                "onlineStatus", False
+            )
+    except Exception as e:
+        print(f"tracker: something went wrong while proccessing wynncraft data: {e}")
 
-    hypixel_mode = hypixel_data.get("mode")  # eg LOBBY, hub
-    if isinstance(hypixel_mode, str):
-        hypixel_mode = hypixel_mode.capitalize()
+    try:
+        hypixel_online = False
+        hypixel_game_type = None
+        hypixel_mode = None
+        if hypixel_response is not None and not isinstance(
+            hypixel_response, HTTPException
+        ):
+            hypixel_data: dict = hypixel_response.get("session", {})
+            hypixel_online = hypixel_data.get("online", False)
+
+            hypixel_game_type = hypixel_data.get(
+                "gameType"
+            )  # eg SKYBLOCK, PROTOTYPE, BEDWARS
+            if isinstance(hypixel_game_type, str):
+                hypixel_game_type = hypixel_game_type.capitalize()
+                hypixel_game_type = hypixel_game_type_map.get(
+                    hypixel_game_type, hypixel_game_type
+                )
+
+            hypixel_mode = hypixel_data.get("mode")  # eg LOBBY, hub
+            if isinstance(hypixel_mode, str):
+                hypixel_mode = hypixel_mode.capitalize()
+                hypixel_mode = hypixel_mode_map.get(hypixel_mode, hypixel_mode)
+    except Exception as e:
+        print(f"tracker: something went wrong while proccessing hypixel data: {e}")
 
     player_status = PlayerStatus(
         wynncraft_restricted=wynncraft_restricted,
@@ -121,22 +201,35 @@ async def get_status(uuid):
 
 
 async def get_wynncraft_status(session, uuid):
+    if "wynncraft" in ignored_sources[uuid]:
+        print("wynncraft is ignored, passing")
+        return None
     dashed_uuid = dashify_uuid(uuid)
     async with session.get(
         f"https://api.wynncraft.com/v3/player/{dashed_uuid}",
         headers={"Authorization": f"Bearer {wynn_token}"},
     ) as response:
+        if response.status == 404:
+            raise exceptions.NotFound()
         response.raise_for_status()
         response: dict = await response.json()
         return response
 
 
 async def get_hypixel_status(session, uuid):
+    if "hypixel" in ignored_sources[uuid]:
+        return None
     headers = {"API-Key": hypixel_api_key}
     params = {"uuid": uuid}
     async with session.get(
         "https://api.hypixel.net/v2/status", params=params, headers=headers
     ) as response:
+        if (
+            response.status == 404
+        ):  # this doesn't ever return 404, TODO implement returning it manually
+            raise exceptions.NotFound()
+        if response.status == 429:
+            raise exceptions.UpstreamError()
         response.raise_for_status()
         return await response.json()
 
